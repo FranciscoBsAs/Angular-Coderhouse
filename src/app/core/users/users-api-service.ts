b35d6b38bf6993b61de2f14e3a93bcc9b66403fa
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-23 17:29:15 -0300

    feature: add UsersAPIService with method to fetch users from API getUsersThroughMockIO ()

---
+++ src/app/core/users/users-api-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { RoutingDB } from '../../../enumRoutesDB';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { userInterface } from '../../../shared/sharedContent/entities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersAPIService {
+
+  private baseURL : string = RoutingDB.ENDPOINT_SECOND_APIio_DB
+
+  constructor( private myHTTP : HttpClient ) { }
+
+
+  getUsersThroughMockIO () : Observable<userInterface[]> {
+
+    return this.myHTTP.get<userInterface[]>(`${this.baseURL}/${RoutingDB.USERS}`)
+
+  }
+
+}
