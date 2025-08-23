232b64dccdb85bac175cac8067c3ae77288a7a97
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-23 16:13:02 -0300

    feature: update Toolbar component to use dynamic title as a property and implement OnInit lifecycle

--- src/app/toolbar/toolbar.ts
+++ src/app/toolbar/toolbar.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BigTitle } from '../../shared/directives/big-title';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,6 +10,27 @@ import { BigTitle } from '../../shared/directives/big-title';
   standalone: true,
   styleUrl: './toolbar.css'
 })
-export class Toolbar {
+export class Toolbar implements OnInit {
 
+  readonly titleApp : string = "College App"
+
+  titleRouting! : string
+
+  ngOnInit(): void { // comentar
+      
+  }
+
+  /*
+  constructor( private theRouter : Router, private activatedRoute : ActivatedRoute ) {}
+
+  ngOnInit(): void {
+
+    this.theRouter.events.subscribe( () => {
+      const currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path
+      //this.titleRouting = this
+    } )
+      
+  }
+
+  */
 }
