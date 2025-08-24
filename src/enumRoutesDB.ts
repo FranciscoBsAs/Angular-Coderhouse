ca6a38d397e7023d42dcb3b11ba2dbc198431595
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-23 22:56:56 -0300

    feature: add the news URL and Endpoint for the users DB API, the second one

--- src/enumRoutesDB.ts
+++ src/enumRoutesDB.ts
@@ -2,7 +2,11 @@ export enum RoutingDB {
 
     EDPOINT_APIio_DB = 'https://68916597447ff4f11fbc72b6.mockapi.io',
 
+    ENDPOINT_SECOND_APIio_DB = 'https://68a5f2ce2a3deed2960f6fd1.mockapi.io',
+
+
     STUDENTS = 'students',
-    COURSES = 'courses'
+    COURSES = 'courses',
+    USERS = 'users'
 
 }
