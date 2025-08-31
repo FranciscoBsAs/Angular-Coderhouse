e1775d52e88896bfc7303bc3750ccf817d2dfb71
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-30 23:37:15 -0300

    feature: enhance app configuration by adding NgRx store and effects for authentication and user management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { authReducer } from './core/authNgRx/auth.reducer';
+import { usersNgRxReducer } from './core/users/usersNgRx/users.reducer';
+import { AuthEffects } from './core/authNgRx/auth.effects';
+import { UsersNgRxEffects } from './core/users/usersNgRx/users.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig : ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    
     //new one, HTTP
     provideHttpClient(),
-  ]
+    provideStore({
+        auth: authReducer,
+        usersNgRx: usersNgRxReducer
+    }),
+    provideEffects([AuthEffects, UsersNgRxEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
