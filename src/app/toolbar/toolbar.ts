9a58352e0b38290a90edc9f614ae820c87a5f291
Francisco T.C <franciscotchiressi@gmail.com>
2025-09-03 22:40:45 -0300

    feature: refactor toolbar component to integrate NgRx to render logged user and update routing title logic to render the title

--- src/app/toolbar/toolbar.ts
+++ src/app/toolbar/toolbar.ts
@@ -1,36 +1,94 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BigTitle } from '../../shared/directives/big-title';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectUser } from '../core/authNgRx/auth.selector';
+import { routeMapingType } from '../../shared/sharedContent/entities';
+import { RoutingPaths } from '../../shared/urlRoutesEnum';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [ CommonModule, BigTitle ],
+  imports: [ CommonModule, BigTitle, RouterModule ],
   templateUrl: './toolbar.html',
   standalone: true,
   styleUrl: './toolbar.css'
 })
 export class Toolbar implements OnInit {
 
+
   readonly titleApp : string = "College App"
 
   titleRouting! : string
 
-  ngOnInit(): void { // comentar
-      
-  }
+  public userNameTitle! : string | null
 
-  /*
-  constructor( private theRouter : Router, private activatedRoute : ActivatedRoute ) {}
+  constructor( private theAuthStore : Store, private theRouter : Router ) {}
 
-  ngOnInit(): void {
 
-    this.theRouter.events.subscribe( () => {
-      const currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path
-      //this.titleRouting = this
+  ngOnInit(): void { 
+    
+    this.theAuthStore.select( selectUser ).subscribe( ( userSelected ) => {
+
+      userSelected == undefined
+                    ? this.userNameTitle = null
+                    : ( this.userNameTitle = userSelected?.userName )
+                    
     } )
+                  
+
+    this.theRouter.events.subscribe( ( event ) => {
+
+      if ( event instanceof NavigationEnd ) { 
+        
+        this.setTitleRouting()
       
+        console.log( 'ruta actual', this.titleRouting )
+      }
+       
+    } )
+
   }
 
-  */
+
+  private readonly routeTitleMap : routeMapingType = {
+
+    [ RoutingPaths.STUDENTS ]: "students-full-component",
+    
+    [ RoutingPaths.VIEW_SINGULAR_STUDENT ]: "view-singular-student",
+
+    [ RoutingPaths.EDIT_SINGULAR_STUDENT ]: "edit-student-form",
+
+    [ RoutingPaths.REGISTRATIONS ]: "registrations-full-component",
+
+    [ RoutingPaths.COURSES ]: "courses-full-component",
+
+    [ RoutingPaths.VIEW_SINGULAR_COURSE ]: "view-singular-course",
+
+    [ RoutingPaths.EDIT_SINGULAR_COURSE ]: "edit-course-form",
+
+    [ RoutingPaths.NOT_FOUND_ROUTE ]: "not-found-component"
+
+  }
+
+
+  private setTitleRouting () {
+
+    const currentURL : string = this.theRouter.url ;
+
+    const foundComparedRoute = Object.keys( this.routeTitleMap ).find( ( routingPathKey ) => (
+
+      currentURL.includes( routingPathKey )
+
+    ) )
+
+
+    this.titleRouting = foundComparedRoute
+                      ? this.routeTitleMap[ foundComparedRoute ]
+                      : ''
+
+  }
+
+
 }
+
