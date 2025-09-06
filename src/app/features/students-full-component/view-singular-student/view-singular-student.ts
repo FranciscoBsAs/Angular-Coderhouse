e42745431f365e6cc2d3c0915d8949c0cc64b927
Francisco T.C <franciscotchiressi@gmail.com>
2025-09-05 23:32:06 -0300

    feature: add course unenrollment functionality in view-singular-student component; implement UnenrollFromCourse method and update view for course list with buttons

--- src/app/features/students-full-component/view-singular-student/view-singular-student.ts
+++ src/app/features/students-full-component/view-singular-student/view-singular-student.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { studentInterface } from '../../../../shared/sharedContent/entities';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -7,19 +7,24 @@ import { MatTable, MatTableModule,  } from '@angular/material/table';
 import { FullNamePipe } from '../../../../shared/pipes/full-name-pipe';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { StudentsAPIService } from '../students-api-service';
+import { MyMatCommonRouterModule } from '../../my-mat-common-router/my-mat-common-router-module';
 
 
 @Component({
   selector: 'app-view-singular-student',
-  imports: [ RouterModule, CommonModule, MatTableModule, FullNamePipe, MatListModule, MatGridListModule ],
+  imports: [ MyMatCommonRouterModule ,RouterModule, CommonModule, MatTableModule, FullNamePipe, MatListModule, MatGridListModule ],
   templateUrl: './view-singular-student.html',
   styleUrl: './view-singular-student.css'
 })
-export class ViewSingularStudent {
+export class ViewSingularStudent  {
 
   aSingularStudent! : studentInterface | undefined ;
 
-  constructor( private theRouter : Router ) {
+  constructor( 
+    private theRouter : Router ,
+    private studentsService : StudentsAPIService
+  ) {
 
     const theNavigation = this.theRouter.getCurrentNavigation();
 
@@ -28,7 +33,38 @@ export class ViewSingularStudent {
   }
 
 
-  columnTitlesSingular : string[] = [ 'FullName', 'DNI', 'Age', 'Average', 'Registered Courses' ]
 
+  public UnenrollFromCourse ( certainAttributedCourse : string ) {
+
+    if( !this.aSingularStudent ) return
+
+
+    const updatedCourses = this.aSingularStudent.courses.filter(
+      (eachCourse) => eachCourse !== certainAttributedCourse
+    )
+
+
+    const updatedSingularStudent : studentInterface = {
+      ...this.aSingularStudent,
+      courses: updatedCourses
+    }
+
+
+    this.studentsService.editStudentInDB( updatedSingularStudent ).subscribe(
+
+      {
+        next: ( student ) => {
+          this.aSingularStudent = student
+        },
+        error: (err) => {
+          console.error( 'Error al desinscribir', err )
+        }
+      }
+
+    )
+
+  }
+
+  columnTitlesSingular : string[] = [ 'FullName', 'DNI', 'Age', 'Average', 'Registered Courses' ]
 
 }
