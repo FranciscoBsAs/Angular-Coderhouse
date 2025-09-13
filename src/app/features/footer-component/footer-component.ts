93ed65c5bf870c7f0b387e7e87b453d5f7ed433d
Francisco T.C <franciscotchiressi@gmail.com>
2025-09-13 13:02:45 -0300

    feature: add footer-component with personal contact

---
+++ src/app/features/footer-component/footer-component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-component',
+  imports: [],
+  templateUrl: './footer-component.html',
+  styleUrl: './footer-component.css'
+})
+export class FooterComponent {
+
+  readonly myPersonalEmail : string = 'franciscotchiressi@gmail.com'
+
+  readonly myPersonalLinkedIn = 'www.linkedin.com/in/francisco-thomas-chiressi-8b55b01a0'
+
+
+}
